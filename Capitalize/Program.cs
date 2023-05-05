30f07155ae757cce27e5d9fa6936558ec75e1d14
ayushi-das <64302051+ayushi-das@users.noreply.github.com>
2023-05-05 19:08:46 +0530

    Add files via upload

---
+++ Capitalize/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capitalize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence :");
+            string s = Console.ReadLine(); 
+            string[] words= s.Split(' ');   
+
+            foreach(string word in words)
+            {
+                char[] letters= word.ToCharArray();
+                letters[0] = char.ToUpper(letters[0]);
+                letters[letters.Length - 1] = char.ToUpper(letters[letters.Length-1]);
+                string Cwords = new string(letters);
+                Console.Write(" "+Cwords);
+            }
+        }
+    }
+}
