bba4d3712867b2861b6c06a06fe288913dce6212
ayushi-das <64302051+ayushi-das@users.noreply.github.com>
2023-05-05 19:18:18 +0530

    Add files via upload

---
+++ PalindromeString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string to check whether it is Palindrome or not");
+            string s=Console.ReadLine();
+            bool isPalindrome = true;
+
+            for(int i=0; i<s.Length/2; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+                    
+            }
+         Console.WriteLine($"The entered string is {(isPalindrome ? "a palindrome" : "not a palindrome")}");
+
+        }
+    }
+}
